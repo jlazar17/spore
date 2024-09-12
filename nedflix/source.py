91808cd875d9a41a5fc3d8b50c7cb2fcc255b27f
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2024-09-12 16:25:16 +0200

    Add source class

---
+++ nedflix/source.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from dataclasses import dataclass
+from typing import Callable, Tuple, Dict
+
+from .coordinates import SkyCoordinate
+from .flux import Flux, flux_from_config
+
+@dataclass(frozen=True)
+class Source:
+    location: SkyCoordinate
+    flux: Flux
+
+def source_from_config(config: Dict) -> Source:
+    location = SkyCoordinate(
+        np.radians(config["location"]["declination"]),
+        np.radians(config["location"]["right_ascension"])
+    )
+    flux = flux_from_config(config["flux"])
+    return Source(location, flux)
