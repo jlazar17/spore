Fix radians/degrees bug

nedflix/detector/detector.py
@@ -4,14 +4,12 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Dict
 
-from ..coordinates import EarthCoordinate
-from ..neutrino import Flavor, NeutrinoType, neutrinos
-from . import units
+from ..conventions import EarthCoordinate, units
 from .detector_response import DetectorResponse, detector_response_from_config
 
-def Medium(Enum):
-    Ice: 1
-    Water: 2
+class Medium(Enum):
+    Ice = 1
+    Water = 2
 
 @dataclass(frozen=True)
 class Detector:
@@ -22,10 +20,10 @@ class Detector:
 
 def detector_from_config(config: Dict) -> Detector:
     location = EarthCoordinate(
-        config["properties"]["latitude"],
-        config["properties"]["longitude"],
+        np.radians(config["properties"]["latitude"]),
+        np.radians(config["properties"]["longitude"]),
     )
-    depth = config["properties"]["depth"] * units.m
+    depth = config["properties"]["depth"] * units.meter
     medium = getattr(Medium, config["properties"]["medium"])
     detector_response = detector_response_from_config(config["response"])
     return Detector(location, depth, medium, detector_response)