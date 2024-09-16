80d71002fcb448d7a764ba59f7b220d1843700a3
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2024-09-16 23:53:54 +0200

    Add function for sampling random misreco direction

--- nedflix/conventions/utils.py
+++ nedflix/conventions/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from typing import Tuple
 from astropy.time import Time
 from astropy.coordinates import EarthLocation, AltAz, SkyCoord
 from astropy import units as u
@@ -43,3 +44,37 @@ def sky_to_local(
     az = np.radians(altaz.az.deg)
     zen = np.pi / 2 - np.radians(altaz.alt.deg)
     return LocalCoordinate(zen, az)
+
+def sample_cone(
+    sc: SkyCoordinate,
+    psi: float
+) -> SkyCoordinate:
+    xyz = sc.to_cartesian()
+    xyz_prime = sample_ring(xyz, psi)
+    dec = np.arcsin(xyz_prime[2])
+    ra = np.arctan2(xyz_prime[1], xyz_prime[0])
+    return SkyCoordinate(dec, ra)
+    
+
+def orthonormal_basis(v: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    if len(v)!=3:
+        raise ValueError("This only works for three vectors")
+    v /= np.linalg.norm(v)
+    # This is the tricky bit.
+    # You can run into dumb numerical issues if you don't do this check
+    if abs(v[0]) < 0.9:
+        v1 = np.array([1.0, 0.0, 0.0])
+    else:
+        v1 = np.array([0.0, 1.0, 0.0])
+    u = np.cross(v, v1)
+    w = np.cross(v, u)
+    u /= np.linalg.norm(u)
+    w /= np.linalg.norm(w)
+    return u, w
+
+def sample_ring(v: np.ndarray, psi: float) -> np.ndarray:
+    v /= np.linalg.norm(v)
+    u, w = orthonormal_basis(v)
+    phi = np.random.uniform(0, 2 * np.pi)
+    point = np.sin(psi) * (np.cos(phi) * u + np.sin(phi) * w) + np.cos(psi)*v
+    return point
