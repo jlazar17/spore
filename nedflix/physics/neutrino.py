7db7d9b7e5f32a252b703264d3043f449e05fce4
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2025-05-05 11:37:20 +0200

    Add __int__ method for neutrino

--- nedflix/physics/neutrino.py
+++ nedflix/physics/neutrino.py
@@ -27,3 +27,6 @@ class Neutrino(Enum):
     NuEBar = NeutrinoDef(-12, NeutrinoType.NuBar, Flavor.Electron)
     NuMuBar = NeutrinoDef(-14, NeutrinoType.NuBar, Flavor.Muon)
     NuTauBar = NeutrinoDef(-16, NeutrinoType.NuBar, Flavor.Tau)
+
+    def __int__(self):
+        return int(self.value)
