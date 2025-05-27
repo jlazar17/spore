216857d0a77a119674d2d804da5e015c6c3232a5
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2025-05-27 18:32:33 +0200

    Expose Source

--- nedflix/source/__init__.py
+++ nedflix/source/__init__.py
@@ -1,4 +1,5 @@
 from ..conventions import units
 from ..physics import Neutrino, neutrinos
+from .source import Source
 from .point_source import PointSource
 from .extended_source import ExtendedSource
